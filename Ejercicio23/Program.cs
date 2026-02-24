d34589178635f9ed6ae03d890cefcabcb0b03af4
dgTr3 <Edugta31@gmail.com>
2026-02-24 16:18:23 -0600

    pide las N notas

--- Ejercicio23/Program.cs
+++ Ejercicio23/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("");
+﻿Console.WriteLine("Cuantos notas va a ingresar?");
+int n=int.Parse(Console.ReadLine());
+double notas = 0;
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Ingrese la nota:");
+    notas += double.Parse(Console.ReadLine());
+}
